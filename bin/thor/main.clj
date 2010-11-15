4ef87be4cf7d01843ecc1e31e423d80dd0a2aac1
unknown <prasannagautam@gmail.com>
2010-11-14 20:08:03 -0500

    thor project stuff

--- bin/thor/main.clj
+++ bin/thor/main.clj
@@ -1,7 +1,14 @@
 (ns thor.main
   (:gen-class)
-  (:use thor.node))
+  (:use thor.node thor.network)
+  )
+
   (defn -main [& args]
+    ( let [ node (create-node 1 "test")
+            node-list (create-node-list)]
+      (println (get-name node))
+      (cons (add-node node node-list) node-list)
+      )
     (println "Hello World"))
   
 
