948e72e9cbb7e58da352b470254eb9c6db0a5c09
Prasanna Gautam <prasannagautam@gmail.com>
2011-04-18 22:29:46 -0400

    data store more or less complete for now -- refactored a bit

--- src/thor/data_store.clj
+++ src/thor/data_store.clj
@@ -2,49 +2,49 @@
   (:require thor.utils))
 ; This is a way to globally store data 
 ; Format
-; :key <HASH>
-; :value <ANYTHING>
-; :nodes (List of nodes)
+; :key <HASH> => { :value <ANYTHING> :nodes (List of nodes)}
 ;
 (defn init-data-store [] (atom {}))
 
 (def *data-store* (init-data-store))
 
-(defstruct data-item :value :nodes)
-(defn init-data-item [v & node-ids] 
-  (struct-map data-item :value v :nodes (ref node-ids))
-  )
+(defstruct data-store-item :value :nodes)
 
-(defn store-data [node-id k v]
-  (let [data-item (get @*data-store* k)]  
-    (if (nil? data-item)
-      (swap! *data-store* assoc k (ref (init-data-item v node-id )))
-      (let [nodes (:nodes @data-item)]
-        (println (str "nodes" @nodes " " node-id))
-        (if (not 
-            (thor.utils/in-seq? node-id
-            @nodes ))
-          (dosync
-          (alter nodes concat (list node-id)))
-      ))
-      )))
+(defn init-data-store-item [v & node-ids] 
+  (struct-map data-store-item :value v :nodes (ref node-ids))
+  )
 
 (defn get-data-item [k]
   (get @*data-store* k)
   )
 
-(defn add-node-to-data-item [item & nodes]
-  ;(if  (nil? (get-data-item (:key item)))
-
-  ;))
+(defn node-contains-data? [data-item node-id]
+  (let [nodes (:nodes data-item)]
+    (thor.utils/in-seq? node-id @nodes)
+    )
   )
 
+(defn get-data-in-node [node-id k]
+  (let [data-item (get-data-item k)]
+    (if (nil? data-item) nil
+      (if (node-contains-data? @data-item node-id)
+        @data-item)
+  )))
+
+
+(defn store-data [node-id k v]
+  (let [data-item (get-data-item k)]  
+    (if (nil? data-item)
+      (swap! *data-store* assoc k (ref (init-data-store-item v node-id )))
+        (if (not (node-contains-data? @data-item node-id))  
+        (dosync
+            (alter (:nodes @data-item) concat (list node-id)))
+          ))
+      ))
 
 
 
-;(defn get-data [node-id k]
-;  (if (contains? @*data-store* k)
-;    (if (contains? )
-;    )
-;  )
-;
+
+
+
+
