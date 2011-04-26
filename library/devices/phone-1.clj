dc7fe12880773213843f9844cee67d33cb75f6a4
prasincs <prasannagautam@gmail.com>
2011-04-26 03:54:36 -0400

    wireless syntax

--- library/devices/phone-1.clj
+++ library/devices/phone-1.clj
@@ -2,6 +2,17 @@
                    :type "phone"
                    :memory "100M"
                    :range "100"
-                   :power {:rx 5 :tx 5} ; in volts
-                   }
+                   :power {:rx 0.01 
+                           :tx 0.01} ; in watts
+                    
+                   :gain {:r 5.15
+                          :t 5.15}
+                    }
+                    ;http://en.wikipedia.org/wiki/Lithium-ion_battery 
+                    :battery {
+                              :type "Li-ion"
+                              :voltage 3.6 
+                              :specific-energy 0.46
+                              :efficiency 0.8 ; 80%
+                             }
            )
--- library/experiments/wireless-test.clj
+++ library/experiments/wireless-test.clj
@@ -2,13 +2,33 @@
 
 (defduration 100)
 
+(defdevice phone-1 {
+                    :type "phone"
+                    :memory "100M"
+                    :range "100"
+                    :power {:rx 5 
+                            :tx 5} ; in volts
+
+                    :gain {:r 5.15 
+                           :t 5.15}
+
+                    :battery {
+                              :type "Li-ion"
+                              :voltage 3.6 
+                              :specific-energy 0.46
+                              :efficiency 0.8 ; 80%
+                              }
+                    }
+
+           )
+
 (defexpt wireless-expt
          {
-          :title "Simple DHT"
-          :devices {:types ["phone-1"]
-                    :number 1e4
-                    :sample 'random
-                    }
+          :title "Wireless Experiment"
+          ;:devices {:types ["phone-1"]
+          ;          :number 1e4
+          ;          :sample 'random
+          ;          }
           :area {:width 100 :height 100}
           :direction 0 ; eventually want to override for particular device
           :speed 0
@@ -30,16 +50,34 @@
             })
 
 
+(def transmitter (create-node 
+                   {:device phone-1 
+                    :location {:x 50 :y 0}} ; start at very top
+                   ))
+
+(def receiver (create-node 
+                {:device phone-1
+                 :location {:x 50 :y 1}}
+                )) ; start at distance 1 away from transmitter
+
+(def power-loss-time (atom ()))
+
+
 (at-start (do 
-            (println "Experiment starting  " 
-                   (get-current-time)
-                     )
-            (println (get-network-attrs))
-            ))
+                 ))
 
-(at-end (println "This is the end my friend  "
-                 (get-current-time) ))
+(at-end (do 
+          (println "Results" )
+          (println @power-loss-time)
+          ))
 
 
+(every 1 (do
+           (move-node receiver + {:x 0 :y 1})
+           (swap! power-loss-time conj 
+                  (:power-received 
+                    (get-message-network-attrs 
+                      (send-message "test" transmitter receiver ))))
+           ))
 
 (simulation-run)
