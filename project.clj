65070e91f3e6e61da100baef091e6c20f57ce3d7
Prasanna Gautam <prasannagautam@gmail.com>
2011-05-01 14:13:11 -0400

    bumped up the version to 0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thor "1.0.0-SNAPSHOT"
+(defproject thor "0.2-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
