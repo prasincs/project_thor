6d30ba9dfdbdc1aef7fab26b38a9c538b5d373cc
prasincs <prasannagautam@gmail.com>
2011-05-09 05:51:57 -0400

    added docs

---
+++ docs/index.clj
@@ -0,0 +1,298 @@
+{:namespaces
+ ({:source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.analytics.experiment-api.html",
+   :name "thor.analytics.experiment",
+   :doc nil}
+  {:source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.core-api.html",
+   :name "thor.core",
+   :doc nil}
+  {:source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.data-store-api.html",
+   :name "thor.data-store",
+   :doc nil}
+  {:source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.demo-api.html",
+   :name "thor.demo",
+   :doc nil}
+  {:source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.dht.aodv-api.html",
+   :name "thor.dht.aodv",
+   :doc "AODV implementation"}
+  {:source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.dht.simple-api.html",
+   :name "thor.dht.simple",
+   :doc nil}
+  {:source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.graph-api.html",
+   :name "thor.graph",
+   :doc nil}
+  {:source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.lang-api.html",
+   :name "thor.lang",
+   :doc "A description parser for thor AKA Hammer"}
+  {:source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.main-api.html",
+   :name "thor.main",
+   :doc nil}
+  {:source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.math-api.html",
+   :name "thor.math",
+   :doc nil}
+  {:source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.messages-api.html",
+   :name "thor.messages",
+   :doc nil}
+  {:source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.net.wireless-api.html",
+   :name "thor.net.wireless",
+   :doc nil}
+  {:source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.network-api.html",
+   :name "thor.network",
+   :doc nil}
+  {:source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.node-api.html",
+   :name "thor.node",
+   :doc
+   "The node class used for defining various nodes in the network"}
+  {:source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.queue-api.html",
+   :name "thor.queue",
+   :doc nil}
+  {:source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.ui.analytics-api.html",
+   :name "thor.ui.analytics",
+   :doc nil}
+  {:source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.ui.animation-api.html",
+   :name "thor.ui.animation",
+   :doc nil}
+  {:source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.ui.node-viewer-api.html",
+   :name "thor.ui.node-viewer",
+   :doc nil}
+  {:source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.ui.processing-test-api.html",
+   :name "thor.ui.processing-test",
+   :doc nil}
+  {:source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.ui.window-api.html",
+   :name "thor.ui.window",
+   :doc nil}
+  {:source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.utils-api.html",
+   :name "thor.utils",
+   :doc nil}
+  {:source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.window-api.html",
+   :name "thor.window",
+   :doc
+   "A placeholder file for now - creates a window and draws nodes"}),
+ :vars
+ ({:arglists ([f]),
+   :name "at-end",
+   :namespace "thor.lang",
+   :source-url nil,
+   :raw-source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.lang-api.html#thor.lang/at-end",
+   :doc "A special at function that just runs at the end",
+   :var-type "macro",
+   :line 182,
+   :file
+   "/home/pgautam/workspace/project_thor_gh_pages/src/thor/lang.clj"}
+  {:arglists ([n attrs]),
+   :name "create-experiment",
+   :namespace "thor.lang",
+   :source-url nil,
+   :raw-source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.lang-api.html#thor.lang/create-experiment",
+   :doc "Create an experiment based on attributes",
+   :var-type "function",
+   :line 130,
+   :file
+   "/home/pgautam/workspace/project_thor_gh_pages/src/thor/lang.clj"}
+  {:arglists ([n attrs]),
+   :name "defdevice",
+   :namespace "thor.lang",
+   :source-url nil,
+   :raw-source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.lang-api.html#thor.lang/defdevice",
+   :doc "Defines a device with name and a map of attributes",
+   :var-type "macro",
+   :line 117,
+   :file
+   "/home/pgautam/workspace/project_thor_gh_pages/src/thor/lang.clj"}
+  {:arglists ([n attrs]),
+   :name "defexpt",
+   :namespace "thor.lang",
+   :source-url nil,
+   :raw-source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.lang-api.html#thor.lang/defexpt",
+   :doc "Defines an experiment",
+   :var-type "macro",
+   :line 198,
+   :file
+   "/home/pgautam/workspace/project_thor_gh_pages/src/thor/lang.clj"}
+  {:arglists ([t attrs]),
+   :name "defmedium",
+   :namespace "thor.lang",
+   :source-url nil,
+   :raw-source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.lang-api.html#thor.lang/defmedium",
+   :doc "Defines the properties of the medium of communication",
+   :var-type "macro",
+   :line 93,
+   :file
+   "/home/pgautam/workspace/project_thor_gh_pages/src/thor/lang.clj"}
+  {:arglists ([attrs]),
+   :name "init-wireless-network",
+   :namespace "thor.lang",
+   :source-url nil,
+   :raw-source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.lang-api.html#thor.lang/init-wireless-network",
+   :doc "Apply the attributes from the map",
+   :var-type "function",
+   :line 56,
+   :file
+   "/home/pgautam/workspace/project_thor_gh_pages/src/thor/lang.clj"}
+  {:arglists ([f]),
+   :name "load-device",
+   :namespace "thor.lang",
+   :source-url nil,
+   :raw-source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.lang-api.html#thor.lang/load-device",
+   :doc "Loads a device from file f.",
+   :var-type "function",
+   :line 227,
+   :file
+   "/home/pgautam/workspace/project_thor_gh_pages/src/thor/lang.clj"}
+  {:arglists ([f]),
+   :name "load-experiment",
+   :namespace "thor.lang",
+   :source-url nil,
+   :raw-source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.lang-api.html#thor.lang/load-experiment",
+   :doc "Load experiment data from file f",
+   :var-type "function",
+   :line 214,
+   :file
+   "/home/pgautam/workspace/project_thor_gh_pages/src/thor/lang.clj"}
+  {:arglists ([from to attrs]),
+   :name "build-message-attrs",
+   :namespace "thor.messages",
+   :source-url nil,
+   :raw-source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.messages-api.html#thor.messages/build-message-attrs",
+   :doc
+   "Build a hash map of the attributes we care about\nlike hops, size of the message, nodes, etc",
+   :var-type "function",
+   :line 32,
+   :file
+   "/home/pgautam/workspace/project_thor_gh_pages/src/thor/messages.clj"}
+  {:arglists ([from to]),
+   :name "calculate-network-attrs",
+   :namespace "thor.net.wireless",
+   :source-url nil,
+   :raw-source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.net.wireless-api.html#thor.net.wireless/calculate-network-attrs",
+   :doc
+   "Calculate the network parameters we'll see if a message \nof certain size were to be sent from point A to point B",
+   :var-type "function",
+   :line 66,
+   :file
+   "/home/pgautam/workspace/project_thor_gh_pages/src/thor/net/wireless.clj"}
+  {:arglists ([freq]),
+   :name "get-wavelength",
+   :namespace "thor.net.wireless",
+   :source-url nil,
+   :raw-source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.net.wireless-api.html#thor.net.wireless/get-wavelength",
+   :doc "Get the wavelength from given frequency in Hz",
+   :var-type "function",
+   :line 41,
+   :file
+   "/home/pgautam/workspace/project_thor_gh_pages/src/thor/net/wireless.clj"}
+  {:arglists ([message from to & [attrs]]),
+   :name "send-network-message",
+   :namespace "thor.net.wireless",
+   :source-url nil,
+   :raw-source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.net.wireless-api.html#thor.net.wireless/send-network-message",
+   :doc "Send a network message from a node to other",
+   :var-type "function",
+   :line 102,
+   :file
+   "/home/pgautam/workspace/project_thor_gh_pages/src/thor/net/wireless.clj"}
+  {:arglists
+   ([{:keys [id name location memory-size device-attrs network-attrs],
+      :or {:device-attrs {}, :network-attrs {}}}]),
+   :name "create-node",
+   :namespace "thor.node",
+   :source-url nil,
+   :raw-source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.node-api.html#thor.node/create-node",
+   :doc "create a node based on given info",
+   :var-type "function",
+   :line 36,
+   :file
+   "/home/pgautam/workspace/project_thor_gh_pages/src/thor/node.clj"}
+  {:arglists ([s]),
+   :name "create-random-queue",
+   :namespace "thor.queue",
+   :source-url nil,
+   :raw-source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.queue-api.html#thor.queue/create-random-queue",
+   :doc "Creates a queue of random events for testing",
+   :var-type "function",
+   :line 36,
+   :file
+   "/home/pgautam/workspace/project_thor_gh_pages/src/thor/queue.clj"}
+  {:arglists ([item & sq]),
+   :name "in-seq?",
+   :namespace "thor.utils",
+   :source-url nil,
+   :raw-source-url nil,
+   :wiki-url
+   "http://prasincs.github.com/project_thor/docs/thor.utils-api.html#thor.utils/in-seq?",
+   :doc "check if an item is in the sequence",
+   :var-type "macro",
+   :line 29,
+   :file
+   "/home/pgautam/workspace/project_thor_gh_pages/src/thor/utils.clj"})}
